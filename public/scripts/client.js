a5c04fa5089b423ebe2621385ccb1a357d9a5928
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-11-29 16:35:37 -0500

    Update client.js
    Updated client.js file to allow to populate the data from fake data following format in index.html for the article "tweet".
    
    Utilizing as well TimeAgo library as well.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,58 +3,68 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
 $(document).ready(function() {
-const createTweetElement = function (tweetObject) {
-    const tweet = tweetObject.content.text;
-    const name = tweetObject.user.name;
-    const avatarsImg = tweetObject.user.avatars;
-    const tweetHandle = tweetObject.user.handle;
-
-    const $createAvatar = $('<img>').attr("src",avatarsImg);
-    const $createName = $('<div>').addClass("user").text(name);
-    const $createTweet = $('<div>').addClass("tweet-body").text(tweet);
-    const $createHandle = $('<p>').addClass("user-handle").text(tweetHandle);
-
-    const $iconDiv1 = $("<div>").addClass("tweet-icons");
-    const $flag = $("<i>").addClass("fas fa-flag");
-    const $iconDiv2 = $("<div>").addClass("tweet-icons");
-    const $retweet = $("<i>").addClass("fas fa-retweet");
-    const $iconDiv3 = $("<div>").addClass("tweet-icons");
-    const $heart = $("<i>").addClass("fas fa-heart");
-
-    const $tweetObject = $("<article>");
-    const $header = $("<header>");
-    const $footer = $("<footer>");
-    const $content = $("<div>").addClass("content");
-    const $dateOfTweet = $("<p>").text(tweetObject['created_at']);
-
-    $iconDiv1.append($flag);
-    $iconDiv2.append($retweet);
-    $iconDiv3.append($heart);
-    $header.append($createAvatar, $createName, $createHandle);
-    $content.append($createTweet);
-    $footer.append($dateOfTweet, $iconDiv1, $iconDiv2, $iconDiv3);
-    $tweetObject.append($header, $content, $footer);
-
-    return $tweetObject;
-};
-});
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
+  const createTweetElement = function(tweet) {
+    let $tweet = `<section id="tweets-container">
+      <article class="tweet">
+        <header>
+          <img src="${tweet.user.avatars}" alt="profile picture">
+          <p class="user">${tweet.user.name}</p>
+          <p class="user-handle">${tweet.user.handle}</p>
+        </header>
+        <div class="tweet-body">
+          <p>${tweet.content.text}</p>
+        </div>
+        <footer>
+          <p class="counter-days">${timeago.format(tweet.created_at)}</p>
+          <div class="tweet-icons">
+            <i class="fas fa-flag"></i>
+          </div>
+          <div class="tweet-icons">
+            <i class="fas fa-retweet"></i>
+          </div>  
+          <div class="tweet-icons">
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+      </section>`;
+    return $tweet;
+  };
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
     "user": {
       "name": "Newton",
       "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
+      "handle": "@SirIsaac"
       },
     "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
     "created_at": 1461116232227
- }
+  };
 
-const $tweet = createTweetElement(tweetData);
+  const tweetData2 = {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1638065358738
+  };
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const $tweet = createTweetElement(tweetData);
+  const $tweet2 = createTweetElement(tweetData2);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+  console.log($tweet2); // to see what it looks like
+  $('#tweets-container').append($tweet2); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+
+});
